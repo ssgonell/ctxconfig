db752bc0e1b73398c598c55dae86769fc2a75217
Shyam Gonella <getshyam@gmail.com>
2019-07-12 23:45:24 -0700

    initial commit with basic search criteria

---
+++ pyconfig.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+
+class Context:
+    def __init__(self, env='', app='', user='', machine=''):
+        self.env = env
+        self.app = app
+        self.user = user
+        self.machine = machine
+
+    def __repr__(self) -> str:
+        return str(vars(self))
+
+
+class Config:
+    def __init__(self, config_file):
+        self._df = pd.read_csv(config_file, keep_default_na=False)
+        print(self._df.to_string())
+
+    def get_value(self, key, context):
+        c = context
+        df = self._df.loc[(self._df['key'] == key)]
+        result = df.loc[(df['env'] == c.env) & (df['app'] == c.app) & (df['user'] == c.user) & (df['machine'] == c.machine)]
+        if len(result) == 1:
+            return result['value'].values[0]
+        result = df.loc[(df['env'] == c.env) & (df['app'] == c.app) & (df['user'] == c.user)]
+        if len(result) == 1:
+            return result['value'].values[0]
+        result = df.loc[(df['env'] == c.env) & (df['app'] == c.app)]
+        if len(result) == 1:
+            return result['value'].values[0]
+        result = df.loc[(df['env'] == c.env)]
+        if len(result) == 1:
+            return result['value'].values[0]
+        result = df
+        if len(result) == 1:
+            return result['value'].values[0]
+
+
+def check_config(config, key, context):
+    value = config.get_value(key, context)
+    print(f'{key} = {value} for context: {context}')
+
+
+def main():
+    config = Config('config_data.csv')
+    check_config(config, 'key1', Context())
+    check_config(config, 'key1', Context('dev'))
+    check_config(config, 'key1', Context('dev', 'app1'))
+    check_config(config, 'key1', Context('dev', user='user1'))
+    check_config(config, 'key1', Context('dev', 'app1', 'user1'))
+    check_config(config, 'key1', Context('dev', 'app1', machine='machine1'))
+    check_config(config, 'key1', Context('dev', 'app1', 'user1', 'machine1'))
+    check_config(config, 'key1', Context('prod'))
+    check_config(config, 'key1', Context('prod', 'app1'))
+    check_config(config, 'key1', Context('prod', user='user1'))
+    check_config(config, 'key1', Context('prod', 'app1', 'user1'))
+    check_config(config, 'key1', Context('prod', 'app1', machine='machine1'))
+    check_config(config, 'key1', Context('prod', 'app1', 'user1', 'machine1'))
+
+
+if __name__ == '__main__':
+    main()
+
